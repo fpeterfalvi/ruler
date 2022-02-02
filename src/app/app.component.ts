9f5171e52ec8e7c487bbba998612d094248b4d89
Ferenc Péterfalvi <fpeterfalvi@gmail.com>
2022-02-02 13:58:53 +0100

    base commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ruler';
+  isBold = false;
+  weightOfFont = this.isBold ? 'bold' : 'normal';
+
+
+  rulers = [
+    {
+      name: 'Harald I',
+      nickname: 'Bluetooth',
+      startOfReign: 958,
+      endOfReign: 986,
+      country: 'Denmark',
+      title: 'king'
+    }, {
+      name: 'John',
+      nickname: 'the Fearless',
+      startOfReign: 1404,
+      endOfReign: 1419,
+      country: 'Burgundy',
+      title: 'duke' 
+    }, {
+      name: 'Ivan',
+      nickname: 'the Terrible',
+      startOfReign: 1547,
+      endOfReign: 1584,
+      country: 'Russia',
+      title: 'tsar'
+    }, {
+      name: 'Arduin',
+      nickname: 'of Ivrea',
+      startOfReign: 1002,
+      endOfReign: 1014,
+      country: 'Italy',
+      title: 'king'
+    }, {
+      name: 'Richard I',
+      nickname: 'the Lionheart',
+      startOfReign: 1189,
+      endOfReign: 1199,
+      country: 'England',
+      title: 'king'
+    }, {
+      name: 'Suleiman',
+      nickname: 'the Magnificent',
+      startOfReign: 1520,
+      endOfReign: 1566,
+      country: 'Turkey',
+      title: 'sultan'
+    }
+];
 }
